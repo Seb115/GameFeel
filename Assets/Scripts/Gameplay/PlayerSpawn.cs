9c1dd5f93a6b4864e1557603401ad144bee0ec86
Jesse Rosendahl <jesse@jessar.nl>
2023-01-21 20:23:32 +0100

    Death thing

--- Assets/Scripts/Gameplay/PlayerSpawn.cs
+++ Assets/Scripts/Gameplay/PlayerSpawn.cs
@@ -1,6 +1,7 @@
 using Platformer.Core;
 using Platformer.Mechanics;
 using Platformer.Model;
+using UnityEngine;
 
 namespace Platformer.Gameplay
 {
@@ -19,9 +20,12 @@ namespace Platformer.Gameplay
             if (player.audioSource && player.respawnAudio)
                 player.audioSource.PlayOneShot(player.respawnAudio);
             player.health.Increment();
+            player.GetComponent<Rigidbody2D>().simulated = true;
             player.Teleport(model.spawnPoint.transform.position);
             player.jumpState = PlayerController.JumpState.Grounded;
+            player.enabled = true;
             player.animator.SetBool("dead", false);
+            DeathThing.instance.ResetThing(player);
             model.virtualCamera.m_Follow = player.transform;
             model.virtualCamera.m_LookAt = player.transform;
             Simulation.Schedule<EnablePlayerInput>(2f);
